ae6400b0d635c3be77507b986667caa168a20a39
CassieHtn <cassandre.hutin@mail.novancia.fr>
2017-10-19 15:18:09 +0200

    rajout image RS
    pour le footer
    + lecteur API

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
 
-$(".test").click(function(){
-  $("header").toggle(1000);
-  $("header").toggle(1000);
+$(".musiques").click(function(){
+
 });
 
 $.getJSON(" https://api.airtable.com/v0/appppJm5kCAs0HjO8/musics?api_key=keynjL8mX74AKNlHU",
 					function(data) {
           console.log(data);
           $.each(data.records, function(key,val)  {
-            console.log(val.fields.title);
-            $('<li>'+val.fields.title+'</li>').appendTo('#musiques');
 
+            $("<div class='col-md-3'><iframe src='https://open.spotify.com/embed?uri="+val.fields['spotify-link']+"&view=coverart' frameborder='0' allowtransparency='true'></iframe></div>").appendTo('#musiques');
 
         });
 
@@ -19,5 +17,14 @@ $.getJSON(" https://api.airtable.com/v0/appppJm5kCAs0HjO8/musics?api_key=keynjL8
       }
 
   );
+
+  $(function(){
+    $(".fancy-button").mousedown(function(){
+      $(this).bind('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function(){
+          $(this).removeClass('active');
+      })
+       $(this).addClass("active");
+    });
+  });
 
 });
